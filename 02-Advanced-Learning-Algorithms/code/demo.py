8904ac76a65762777c1a9ff66a11a4a8e86c4246
paudelsamir <samirpaudel2005@gmail.com>
2025-01-26 20:10:35 +0545

    Update demo.py

--- 02-Advanced-Learning-Algorithms/code/demo.py
+++ 02-Advanced-Learning-Algorithms/code/demo.py
@@ -1,31 +1,22 @@
-import numpy as np
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-import matplotlib.pyplot as plt
-from sklearn import tree
-
-# Load the Iris dataset
-iris = load_iris()
-X = iris.data
-y = iris.target
-
-# Split the data into training and testing sets (80% training, 20% testing)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Create a Decision Tree model with specific hyperparameters
-clf = DecisionTreeClassifier(criterion='gini', max_depth=3, min_samples_split=2, random_state=42)
-clf.fit(X_train, y_train)
-
-# Predict on the test set
-y_pred = clf.predict(X_test)
-
-# Calculate the accuracy of the model
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy of the Decision Tree Classifier: {accuracy * 100:.2f}%")
-
-# Visualize the trained Decision Tree
-plt.figure(figsize=(12,8))
-tree.plot_tree(clf, filled=True, feature_names=iris.feature_names, class_names=iris.target_names)
-plt.show()
+class KNN:
+    def __init__(self, k):
+        self.k = k
+    
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+    
+    def euclidean_distance(self, x1, x2):
+        return np.sqrt(np.sum((x1 - x2) ** 2))
+    
+    def predict(self, X_test):
+        predictions = []
+        for i in range (len(X_test)):
+            distances = [self.euclidean_distance(X_test[i], x_train) for x_train in self.X_train] #calculate the distance between the test point and all the training points
+            k_indices = np.argsort(distances)[:self.k] #get the indices of the k nearest points
+            k_nearest_labels = [self.y_train[i] for i in k_indices] #get the labels of the k nearest points
+            most_common = np.bincount(k_nearest_labels).argmax()
+            #most_common = Counter(k_nearest_labels).most_common(1)[0][0]
+            predictions.append(most_common) 
+        return predictions
+    
